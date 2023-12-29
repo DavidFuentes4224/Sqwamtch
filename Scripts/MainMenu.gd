0828d88600f5b03f05b7e3336b588878d2e8b5f1
DavidFuentes4224 <davidfuentes4224@yahoo.com>
2023-12-29 15:02:51 -0800

    Added main menu scene

---
+++ Scripts/MainMenu.gd
@@ -0,0 +1,21 @@
+extends CanvasLayer
+
+var mainScene = preload("res://Nodes/Main.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_packed(mainScene)
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
